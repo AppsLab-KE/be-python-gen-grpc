613a0e14c1aa7ccaeb6b9409267faa79d89045a9
Marvin Collins <marvin.collins@scaratec.com>
2023-04-06 14:37:28 +0300

    simple grpc package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Apps:Lab everyshillings',
+    version='0.1.0',
+    description='Apps:Lab everyshillings',
+    author='Marvin Collins',
+    author_email='marvin@appslab.co.ke',
+    url='https://github.com/AppsLab-KE/be-python-gen-grpc',
+    packages=find_packages(),
+    install_requires=[
+        'grpcio-tools',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+
